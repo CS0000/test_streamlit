a8f31e9ec889627efb2d3114f3b0ce60db3fb120
CS0000 <chenshuosusan@163.com>
2022-12-07 23:30:14 +0100

    update

--- scripts/st_vis.py
+++ scripts/st_vis.py
@@ -6,6 +6,7 @@ import plotly
 import plotly.graph_objects as go
 import plotly.express as px
 import time
+import numpy as np
 
 
 # read df for plotly
@@ -13,7 +14,7 @@ import time
 df = pack(file_name='data/test_df.pkl',mode='rb')
 # la	lo	hover	date_belong location  raw_location
 # st.write(df.head())
-plot_spot = st.empty()
+# plot_spot = st.empty()
 # date range slider 
 date_list = []
 for i in df['date_belong'].tolist():
@@ -36,9 +37,28 @@ for i in date_list:
 
 # st.write(select_date)
 
+st.title("Plotly Graphs with containers")
+
+chart_data = pd.DataFrame(np.random.randn(20, 3),columns=['a', 'b', 'c'])
+
+plot_spot = st.empty() # holding the spot for the graph
+
+#make the widgets that will change the graph
+line = st.selectbox("Choose a column:", chart_data.columns)
+
+title = st.radio("Decide to include a title:", ["Yes", "No"])
+
+#now code the plotly chart based on the widget selection
+fig = px.line(chart_data, x=chart_data.index, y=line)
+
+if title == "Yes":
+    fig.update_layout(title="My graph title")
+
+#send the plotly chart to it's spot "in the line" 
+with plot_spot:
+    st.plotly_chart(fig)
 
 def vis(se):
-    global plot_spot
     df_select = df.loc[df['date_belong'].isin(se)]
     d = go.Scattermapbox(lat = df_select['la'],
                             lon = df_select['lo'],
@@ -52,10 +72,11 @@ def vis(se):
     Fig.update_layout(mapbox=dict(style='open-street-map',
                                 center=dict(lat=52.3,lon=4.9),
                                 zoom=5
+                                ),
+                      margin={"r":0,"t":0,"l":0,"b":0},
+                      legend=dict(y=0.2,x=1)
                                 )
-                                )
-    Fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
-    Fig.update_layout(legend=dict(y=0.2,x=1))
+
 
     st.write(date_range)
     st.write(se)
@@ -63,7 +84,7 @@ def vis(se):
     with plot_spot:
         st.plotly_chart(Fig)
 
-vis(select_date)
+# vis(select_date)
 
 # # Fig.write_html('test.html')
 # st.plotly_chart(Fig)
