dc33092c5dff7e9d289d348bf7b9b57a609867e5
CS0000 <chenshuosusan@163.com>
2022-12-08 19:48:36 +0100

    update

--- scripts/st_vis.py
+++ scripts/st_vis.py
@@ -4,7 +4,7 @@ from helper import *
 import pandas as pd
 import plotly
 import plotly.graph_objects as go
-# import plotly.express as px
+import plotly.express as px
 
 
 
@@ -30,22 +30,26 @@ d1,d2 = st.slider(
 
 df_select = df.loc[(df['date_belong']<=d2)&(df['date_belong']>=d1),:]
 
-fig = go.Figure(data=go.Scattermapbox(lat = df_select['la'],
-                            lon = df_select['lo'],
-                            mode='markers',
-                            marker = dict(size=12,color='black'), 
-                            text = df_select['hover'].tolist(),
-                            hoverinfo='text'))
-fig.update_layout(mapbox=dict(style='open-street-map',
-                                center=dict(lat=52.3,lon=4.9),
-                                zoom=5
-                                ),
-                      margin={"r":0,"t":0,"l":0,"b":0},
-                      legend=dict(y=0.2,x=1)
-                                )
+Fig = px.scatter_mapbox(df_select,lat='la',lon='lo',
+                        hover_name='hover',
+                        mapbox_style='open-street-map',
+                        center=dict(lat=52.3,lon=4.9))
+# fig = go.Figure(data=go.Scattermapbox(lat = df_select['la'],
+#                             lon = df_select['lo'],
+#                             mode='markers',
+#                             marker = dict(size=12,color='black'), 
+#                             text = df_select['hover'].tolist(),
+#                             hoverinfo='text'))
+# fig.update_layout(mapbox=dict(style='open-street-map',
+#                                 center=dict(lat=52.3,lon=4.9),
+#                                 zoom=5
+#                                 ),
+#                       margin={"r":0,"t":0,"l":0,"b":0},
+#                       legend=dict(y=0.2,x=1)
+#                                 )
 
 with plot_spot:
-    st.plotly_chart(fig)
+    st.plotly_chart(Fig)
 
 
 
